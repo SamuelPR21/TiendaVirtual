dcf45886f2ada4574f630b1735f24ab57bf3465d
Juanher01 <u20222209033@usco.edu.co>
2025-05-20 23:02:13 -0500

    api google maps

--- client/src/components/PuntosVenta/PuntosVenta.js
+++ client/src/components/PuntosVenta/PuntosVenta.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PuntosVenta.css';
 import NavbarGeneral from '../Navbar/NavbarGeneral';
 import Footer from '../Home/Footer/Footer';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const importarImagenes = (contexto) => {
   const imagenes = {};
@@ -14,67 +15,125 @@ const importarImagenes = (contexto) => {
 
 const imagenesPuntos = importarImagenes(require.context('./imgPuntos', false, /\.(png|jpe?g|svg)$/));
 
-
 const puntos = [
   {
     nombre: 'Sucursal Norte',
     direccion: 'Calle 123 #45-67, Bogotá',
     telefono: '301 123 4567',
+    lat: 4.710989, 
+    lng: -74.072090,
     imagen: imagenesPuntos['Carniceria1'],
   },
   {
     nombre: 'Sucursal Centro',
     direccion: 'Cra 10 #20-30, Medellín',
     telefono: '302 765 4321',
+    lat: 6.244203,
+    lng: -75.581215,
     imagen: imagenesPuntos['Carniceria2'],
   },
   {
     nombre: 'Sucursal Sur',
     direccion: 'Av. 1ra #10-10, Cali',
     telefono: '300 111 2222',
+    lat: 3.451647,
+    lng: -76.531985,
     imagen: imagenesPuntos['Carniceria3'],
   },
 ];
 
+const mapContainerStyle = {
+  width: '100%',
+  height: '400px',
+};
+
+const defaultCenter = {
+  lat: 4.710989,
+  lng: -74.072090,
+};
+
 function PuntosVenta() {
+  const [mapRef, setMapRef] = useState(null);
+
+  const onLoad = map => {
+    setMapRef(map);
+  };
+
+  const handleVerEnMapa = (lat, lng) => {
+    if (mapRef) {
+      const target = { lat, lng };
+
+
+      mapRef.panTo(target);
+
+     
+      setTimeout(() => {
+        mapRef.setZoom(14); 
+      }, 500); 
+    }
+  };
+
   return (
     <div className="main-container">
       <NavbarGeneral />
-    <section className="puntos-venta-container">
-      <div className="content">
-        <br />
-        <br />
-        <h1 className="title has-text-centered">Nuestros Puntos de Venta</h1>
 
-        <div className="columns is-multiline">
-          {puntos.map((punto, index) => (
-            <div className="column is-one-third" key={index}>
-              <div className="card punto-card">
-                <div className="card-image">
-                  <figure className="image is-4by3">
-                    <img src={punto.imagen} alt={punto.nombre} />
-                  </figure>
+      <section className="puntos-venta-container">
+        <div className="content">
+          <h1 className="title has-text-centered">Nuestros Puntos de Venta</h1>
+
+          <LoadScript googleMapsApiKey="AIzaSyCV4XAfdYgw94lNJra617EP2VuY4fCIzZQ">
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              center={defaultCenter}
+              zoom={6}
+              onLoad={onLoad}
+              options={{
+                mapTypeControl: false,
+                streetViewControl: false,
+              }}
+            >
+              {puntos.map((punto, index) => (
+                <Marker
+                  key={index}
+                  position={{ lat: punto.lat, lng: punto.lng }}
+                  title={punto.nombre}
+                />
+              ))}
+            </GoogleMap>
+          </LoadScript>
+
+          <br />
+
+          <div className="columns is-multiline">
+            {puntos.map((punto, index) => (
+              <div className="column is-one-third" key={index}>
+                <div className="card punto-card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={punto.imagen} alt={punto.nombre} />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <p className="title is-5">{punto.nombre}</p>
+                    <p><strong>Dirección:</strong> {punto.direccion}</p>
+                    <p><strong>Teléfono:</strong> {punto.telefono}</p>
+                  </div>
+                  <footer className="card-footer">
+                    <a href={`tel:${punto.telefono.replace(/\s/g, '')}`} className="card-footer-item button is-link is-light">
+                      Llamar
+                    </a>
+                    <button onClick={() => handleVerEnMapa(punto.lat, punto.lng)} className="card-footer-item button is-info is-light">
+                      Ver en mapa
+                    </button>
+                  </footer>
                 </div>
-                <div className="card-content">
-                  <p className="title is-5">{punto.nombre}</p>
-                  <p><strong>Dirección:</strong> {punto.direccion}</p>
-                  <p><strong>Teléfono:</strong> {punto.telefono}</p>
-                </div>
-                <footer className="card-footer">
-                  <a href={`tel:${punto.telefono.replace(/\s/g, '')}`} className="card-footer-item button is-link is-light">
-                    Llamar
-                  </a>
-                  <a href={`https://maps.google.com/?q=${encodeURIComponent(punto.direccion)}`} target="_blank" rel="noreferrer" className="card-footer-item button is-info is-light">
-                    Ver en mapa
-                  </a>
-                </footer>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-    <Footer className="footer" />
+      </section>
+
+      <Footer className="footer" />
     </div>
   );
 }
