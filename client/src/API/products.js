25fa9dca9b5999f79a159d9642604315a965c13c
Juanher01 <u20222209033@usco.edu.co>
2025-11-02 21:24:57 -0500

    Se agrega en API products, se modifica el componente de productos y se elimina la carpeta imgProductos

---
+++ client/src/API/products.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { API_PRODUCTS } from "../utils/constans";
+
+// Listar todos (o con query ?animal= si lo envías)
+export const fetchProducts = async (animal) => {
+  const url = animal
+    ? `${API_PRODUCTS}?animal=${encodeURIComponent(animal)}`
+    : API_PRODUCTS;
+
+  const { data } = await axios.get(url); 
+  return data; 
+};
+
+// Filtrar por animal usando /animal/:animal
+export const fetchProductsByAnimal = async (animal) => {
+  const { data } = await axios.get(
+    `${API_PRODUCTS}/animal/${encodeURIComponent(animal)}`
+  );
+  return data;
+};
+
+// Obtener por id
+export const fetchProductById = async (id) => {
+  const { data } = await axios.get(`${API_PRODUCTS}/${id}`);
+  return data; 
+};
